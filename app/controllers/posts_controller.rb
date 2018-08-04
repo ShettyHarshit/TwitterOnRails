9b853116375992810bb778256cc628c8c1a3eba7
ShettyHarshit <swaharsh@gmail.com>
2018-08-04 20:25:05 +0530

    Add[Tweet Structure]

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,25 @@ class PostsController < ApplicationController
     before_action :authenticate_user!
   def index
     @user = current_user.id
+    @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to '/posts#index'
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:body)
+    end
 end
